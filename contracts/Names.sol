203dd0e569038cab25d7ecc838d0adadeea24fb1
Carlos Colorado <carlos.colorado@gmail.com>
2022-03-27 11:35:31 -0600

    Basic app
    * Adds the basic name registry with test cases

--- contracts/Names.sol
+++ contracts/Names.sol
@@ -1,6 +1,198 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Names {
-    constructor() public {}
+    using SafeMath for uint256;
+
+    uint256 public constant MIN_NAME_LENGTH = 3;
+
+    uint256 public pricePerChar;
+    mapping(bytes32 => bool) private registered;
+    mapping(bytes32 => address) private nameOwner;
+    mapping(bytes32 => uint256) private expirity;
+    mapping(bytes32 => uint256) private lockedBalance;
+
+    event WithdrawnFunds(address indexed user, uint256 amount, bool ejected);
+    event NameReserved(
+        bytes32 indexed _name,
+        address indexed user,
+        uint256 blockAmount
+    );
+
+    /// @notice  does something
+    /// @param _pricePerChar uint256 price per name character per block
+    constructor(uint256 _pricePerChar) {
+        require(_pricePerChar > 0, "invalid price per char");
+        pricePerChar = _pricePerChar;
+    }
+
+    function _getNameKey(string memory _name) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_name));
+    }
+
+    /// @notice gets the registered address for a name
+    /// @param _name string name value
+    /// @return owner address
+    function resolve(string memory _name) public view returns (address owner) {
+        bytes32 _nameKey = _getNameKey(_name);
+        if (isRegistered(_name)) {
+            return nameOwner[_nameKey];
+        }
+    }
+
+    /// @notice returns the expliration date of a given name
+    /// @param _name string value of the name
+    /// @return _expirationBlock uint256
+    function getExpiration(string memory _name)
+        public
+        view
+        returns (uint256 _expirationBlock)
+    {
+        return expirity[_getNameKey(_name)];
+    }
+
+    /// @notice returns the locked balance for a given name
+    /// @param _name string value of the name
+    /// @return _lockedBalance uint256
+    function getLockedBalance(string memory _name)
+        public
+        view
+        returns (uint256 _lockedBalance)
+    {
+        return lockedBalance[_getNameKey(_name)];
+    }
+
+    /// @notice checks if a domain currently registered
+    /// @param _name string name to check
+    /// @return result
+    function isRegistered(string memory _name)
+        public
+        view
+        returns (bool result)
+    {
+        bytes32 _nameKey = _getNameKey(_name);
+        return expirity[_nameKey] > block.number && registered[_nameKey];
+    }
+
+    /// @notice transfers the locked funds to its owner
+    /// @dev internal function
+    /// @param _name string name to check
+    /// @return _wallet address of the user recieving funds
+    /// @return _balance uint256 amount being ejected
+    function _ejectLockedAmount(bytes32 _nameKey)
+        internal
+        returns (address payable _wallet, uint256 _balance)
+    {
+        _balance = lockedBalance[_nameKey];
+        require(_balance > 0, "Insufficient withdraw balance");
+
+        lockedBalance[_nameKey] = 0;
+
+        _wallet = payable(nameOwner[_nameKey]);
+        _wallet.transfer(_balance);
+    }
+
+    /// @notice allows owner to withdraw balances once unlocked
+    /// @dev emits event WithdrawnFunds
+    /// @param _name string name value
+    function withdraw(string memory _name) external {
+        bytes32 _nameKey = _getNameKey(_name);
+        require(msg.sender == nameOwner[_nameKey], "Not the name owner");
+        require(expirity[_nameKey] < block.number, "Balance is locked");
+
+        (address _recipient, uint256 _balance) = _ejectLockedAmount(_nameKey);
+
+        registered[_nameKey] = false;
+        nameOwner[_nameKey] = address(0);
+        expirity[_nameKey] = 0;
+
+        emit WithdrawnFunds(_recipient, _balance, false);
+    }
+
+    /// @notice calculates the price for a given name to regitry
+    /// @param _name name to calculate the price for
+    /// @param _blockCount uint256 amouint of blocks to register
+    /// @return price uint256
+    function getRegistryPrice(string memory _name, uint256 _blockCount)
+        public
+        view
+        returns (uint256 price)
+    {
+        uint256 nameLength = bytes(_name).length;
+        return nameLength.mul(_blockCount).mul(pricePerChar);
+    }
+
+    /// @notice calculte if there is an excess for a registration payment
+    /// @dev  internal function
+    /// @param _price uint256 price for the registration
+    /// @param _lockedBalance uint256 amount to consider to be already locked
+    /// @param _value uint256 value payed on the transaction
+    /// @param _payee address receiving the excess payment
+    /// @return
+    function _settlePayment(
+        uint256 _price,
+        uint256 _lockedBalance,
+        uint256 _value,
+        address _payee
+    ) internal {
+        uint256 _balance = _lockedBalance.add(_value);
+        require(_balance >= _price, "Payment is insufficient");
+
+        // sends payment excess
+        if (_balance > _price) {
+            address payable wallet = payable(_payee);
+            wallet.transfer(_balance.sub(_price));
+        }
+    }
+
+    /// @notice renews a registered name
+    /// @dev must send required payment in ether
+    /// @dev emits NameReserved
+    /// @param _name string value of the name
+    /// @param _blockCount uint256 amount of blocks to be registred for
+    function renew(string memory _name, uint256 _blockCount) external payable {
+        bytes32 _nameKey = _getNameKey(_name);
+        require(msg.sender == nameOwner[_nameKey], "Not the name owner");
+
+        uint256 _price = getRegistryPrice(_name, _blockCount);
+        uint256 _lockedBalance = lockedBalance[_nameKey];
+
+        _settlePayment(_price, _lockedBalance, msg.value, msg.sender);
+
+        expirity[_nameKey] = (block.number).add(_blockCount);
+
+        emit NameReserved(bytes(_name), msg.sender, _blockCount);
+    }
+
+    /// @notice registers a name to the message sender
+    /// @dev must send required payment in ether
+    /// @dev emits NameReserved
+    /// @param _name string name value
+    /// @param _blockCount uint256 amount of blocks to register for
+    function register(string memory _name, uint256 _blockCount) public payable {
+        uint256 _price = getRegistryPrice(_name, _blockCount);
+
+        require(msg.value >= _price, "Payment is insufficient");
+
+        bytes32 _nameKey = _getNameKey(_name);
+        require(expirity[_nameKey] < block.number, "Can not register twice");
+
+        if (lockedBalance[_nameKey] > 0) {
+            (address _recipient, uint256 _balance) = _ejectLockedAmount(
+                _nameKey
+            );
+            emit WithdrawnFunds(_recipient, _balance, true);
+        }
+
+        registered[_nameKey] = true;
+        nameOwner[_nameKey] = msg.sender;
+        expirity[_nameKey] = (block.number).add(_blockCount);
+        lockedBalance[_nameKey] = _price;
+
+        _settlePayment(_price, 0, msg.value, msg.sender);
+
+        emit NameReserved(bytes(_name), msg.sender, _blockCount);
+    }
 }
--- test/names.js
+++ test/names.js
@@ -1,13 +1,289 @@
 const Names = artifacts.require("Names");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("Names", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Names.deployed();
-    return assert.isTrue(true);
+const BigNumber = require("bignumber.js");
+const { time } = require("@openzeppelin/test-helpers");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .use(require("chai-bignumber")(BigNumber))
+  .should();
+
+const pricePerChar = 10;
+const blocksInADay = 6400;
+const blocksInAYear = blocksInADay * 365;
+const zeroAddress = "0x0000000000000000000000000000000000000000";
+
+let names;
+let tenLetterWord = "Friendship";
+
+contract("Names", async (accounts) => {
+  const USER_1 = accounts[1];
+  const USER_2 = accounts[2];
+
+  beforeEach(async function () {
+    names = await Names.new(pricePerChar);
+  });
+
+  describe("Unregistered Names", async function () {
+    beforeEach(async function () {
+      // init contract
+    });
+
+    it("block certain balance when registering a name", async function () {
+      let userBalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+      await names.register(tenLetterWord, 10, {
+        value: 1000,
+        from: USER_1,
+        gasPrice: 0,
+      });
+
+      let contractBalance = BigNumber(await web3.eth.getBalance(names.address));
+      contractBalance.should.be.bignumber.equal(1000);
+
+      let userBalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+
+      userBalanceEnd.should.be.bignumber.equal(userBalanceStart.minus(1000));
+    });
+
+    it("return balance excess", async function () {
+      let paymentExcess = 10000;
+      let userBalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+
+      await names.register(tenLetterWord, 10, {
+        value: 1000 + paymentExcess,
+        from: USER_1,
+        gasPrice: 0,
+      });
+
+      let contractBalance = BigNumber(await web3.eth.getBalance(names.address));
+      let userBalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+
+      userBalanceEnd.should.be.bignumber.equal(userBalanceStart.minus(1000));
+      contractBalance.should.be.bignumber.equal(1000);
+    });
+
+    it("should register a name for a certain amount of time", async function () {
+      await names.register(tenLetterWord, 10, { value: 1000 });
+      let expiration = BigNumber(await names.getExpiration(tenLetterWord));
+
+      let currentBlock = await web3.eth.getBlock("latest");
+      let expectedExpiration = currentBlock.number + 10;
+
+      expiration.should.be.bignumber.equal(expectedExpiration);
+    });
+
+    it("not let register the name again", async function () {
+      await names.register(tenLetterWord, 10, { value: 1000 });
+      await names
+        .register(tenLetterWord, 10, { value: 1000 })
+        .should.be.rejectedWith(Error, "Can not register twice");
+    });
+
+    it("resolve to 0x0", async function () {
+      let nameOwner = await names.resolve("randomName");
+      nameOwner.should.be.equal(zeroAddress);
+    });
+
+    it("not register a name if payment is insufficient", async function () {
+      await names
+        .register(tenLetterWord, 10, { value: 100 })
+        .should.be.rejectedWith(Error, "Payment is insufficient");
+
+      let isRegistered = await names.isRegistered(tenLetterWord);
+      isRegistered.should.be.equal(false);
+    });
+
+    it("register a name if payment is insufficient", async function () {
+      await names.register(tenLetterWord, 10, { value: 1000, from: USER_1 });
+      isRegistered = await names.isRegistered(tenLetterWord);
+      nameOwner = await names.resolve(tenLetterWord);
+
+      isRegistered.should.be.equal(true);
+      nameOwner.should.be.equal(USER_1);
+    });
+
+    it("charge configured amout per character", async function () {
+      let charge = BigNumber(await names.getRegistryPrice(tenLetterWord, 10));
+      charge.should.be.bignumber.equal(1000);
+      let chargePerDay = BigNumber(
+        await names.getRegistryPrice(tenLetterWord, blocksInADay)
+      );
+      chargePerDay.should.be.bignumber.equal(640000);
+      let chargePerYear = BigNumber(
+        await names.getRegistryPrice(tenLetterWord, blocksInAYear)
+      );
+      chargePerYear.should.be.bignumber.equal(233600000);
+    });
+  });
+
+  describe("Registered Names", async function () {
+    beforeEach(async function () {
+      await names.register(tenLetterWord, 10, { value: 1000, from: USER_1 });
+    });
+
+    it("be renewable by name owner before expiration", async function () {
+      await names.renew(tenLetterWord, 10, { from: USER_1 });
+      let currentBlock = await web3.eth.getBlockNumber();
+
+      let newExpiration = BigNumber(await names.getExpiration(tenLetterWord));
+      newExpiration.should.be.bignumber.equal(currentBlock + 10);
+    });
+
+    it("should reuse locked balance when renewing", async function () {
+      let userBalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+      let contractBalanceStart = BigNumber(
+        await web3.eth.getBalance(names.address)
+      );
+
+      await names.renew(tenLetterWord, 10, {
+        value: 0,
+        from: USER_1,
+        gasPrice: 0,
+      });
+
+      let contractBalanceEnd = BigNumber(
+        await web3.eth.getBalance(names.address)
+      );
+      let userBalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+
+      userBalanceEnd.should.be.bignumber.equal(userBalanceStart);
+      contractBalanceStart.should.be.bignumber.equal(1000);
+      contractBalanceEnd.should.be.bignumber.equal(1000);
+    });
+
+    it("renew return locked balance", async function () {
+      let userBalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+      let contractBalanceStart = BigNumber(
+        await web3.eth.getBalance(names.address)
+      );
+
+      await names.renew(tenLetterWord, 11, {
+        value: 200,
+        from: USER_1,
+        gasPrice: 0,
+      });
+
+      let contractBalanceEnd = BigNumber(
+        await web3.eth.getBalance(names.address)
+      );
+      let userBalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+
+      userBalanceEnd.should.be.bignumber.equal(userBalanceStart.minus(100));
+      contractBalanceStart.should.be.bignumber.equal(1000);
+      contractBalanceEnd.should.be.bignumber.equal(1100);
+    });
+
+    it("not be renewable by other users", async function () {
+      await names
+        .renew(tenLetterWord, 10, { value: 1000, from: USER_2 })
+        .should.be.rejectedWith(Error, "Not the name owner");
+    });
+
+    it("resolve to name owner address when registered and not expired", async function () {
+      let nameOwner = await names.resolve(tenLetterWord);
+      nameOwner.should.be.equal(USER_1);
+    });
+
+    it("unalbe to withdraw locked balance", async function () {
+      await names
+        .withdraw(tenLetterWord, { from: USER_1, gasPrice: 0 })
+        .should.be.rejectedWith(Error, "Balance is locked");
+    });
+  });
+
+  describe("Expired Names", async function () {
+    beforeEach(async function () {
+      await names.register(tenLetterWord, 10, { value: 1000, from: USER_1 });
+      let currentBlock = await web3.eth.getBlockNumber();
+      await time.advanceBlockTo(currentBlock + 10);
+    });
+
+    it("lose ownership", async function () {
+      let isOwner = await names.isRegistered(tenLetterWord);
+      isOwner.should.be.equal(false);
+    });
+
+    it("renew expired by original user", async function () {
+      await names.renew(tenLetterWord, 10, { value: 1000, from: USER_1 });
+    });
+
+    it("withdraw locked balance after expired", async function () {
+      let userBalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+
+      let lockedBalance = BigNumber(
+        await names.getLockedBalance(tenLetterWord)
+      );
+
+      await names.withdraw(tenLetterWord, { from: USER_1, gasPrice: 0 });
+      await time.advanceBlock();
+
+      let userBalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+
+      userBalanceEnd.should.be.bignumber.equal(
+        userBalanceStart.plus(lockedBalance)
+      );
+    });
+
+    it("unlock balance only by name owner", async function () {
+      await names
+        .withdraw(tenLetterWord, { from: USER_2, gasPrice: 0 })
+        .should.be.rejectedWith(Error, "Not the name owner");
+    });
+
+    it("deregister name when unlocked", async function () {
+      await names.withdraw(tenLetterWord, { from: USER_1, gasPrice: 0 });
+
+      let expiration = BigNumber(await names.getExpiration(tenLetterWord));
+      let isRegistered = await names.isRegistered(tenLetterWord);
+      let owner = await names.resolve(tenLetterWord);
+      let lockedBalance = BigNumber(
+        await names.getLockedBalance(tenLetterWord)
+      );
+
+      expiration.should.be.bignumber.equal(0);
+      isRegistered.should.be.equal(false);
+      owner.should.be.equal(zeroAddress);
+      lockedBalance.should.be.bignumber.equal(0);
+    });
+
+    it("be able to be registered by anyone", async function () {
+      names.register(tenLetterWord, 10, { value: 1000, from: USER_2 });
+    });
+
+    it("resolve to 0x0 when expired", async function () {
+      let nameOwner = await names.resolve(tenLetterWord);
+      nameOwner.should.be.equal(zeroAddress);
+    });
+
+    it("not renewable by new user", async function () {
+      await names
+        .renew(tenLetterWord, 10, { value: 1000, from: USER_2 })
+        .should.be.rejectedWith(Error, "Not the name owner");
+    });
+
+    it("registrable by other users", async function () {
+      await names.register(tenLetterWord, 10, { value: 1000, from: USER_2 });
+    });
+
+    it("eject original user locked balance when registering with a new user", async function () {
+      let user1BalanceStart = BigNumber(await web3.eth.getBalance(USER_1));
+      let user2BalanceStart = BigNumber(await web3.eth.getBalance(USER_2));
+
+      let lockedAmount = BigNumber(await names.getLockedBalance(tenLetterWord));
+
+      await names.register(tenLetterWord, 10, {
+        value: 1000,
+        from: USER_2,
+        gasPrice: 0,
+      });
+
+      let user1BalanceEnd = BigNumber(await web3.eth.getBalance(USER_1));
+      let user2BalanceEnd = BigNumber(await web3.eth.getBalance(USER_2));
+
+      user1BalanceEnd.should.be.bignumber.equal(
+        user1BalanceStart.plus(lockedAmount)
+      );
+      user2BalanceEnd.should.be.bignumber.equal(user2BalanceStart.minus(1000));
+    });
   });
 });
